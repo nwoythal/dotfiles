7328f204532d9f4fb308d98a3dc7507d2dfaf818
Nate W <nwoythal@gmail.com>
2024-04-20 17:54:33 +0200

    Update bootstrap.sh to use rsync instead of cp

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,7 @@ set -euxo pipefail
 
 TEMP=$(mktemp -d)
 git clone https://github.com/nwoythal/dotfiles.git --recurse-submodules $TEMP
-cp -R $TEMP/.tmux/ ~/.tmux/
-cp -R $TEMP/.config/ ~/.config/
-cp -R $TEMP/.includes/ ~/.includes/
-cp -R $TEMP/ohmyzsh/ ~/.oh-my-zsh/
+rsync -r -l $TEMP/.config/ ~/.config/
+rsync -r -l $TEMP/.includes/ ~/.includes/
+rsync -r -l $TEMP/ohmyzsh/ ~/.oh-my-zsh/
 cp $TEMP/.zshrc ~/.zshrc
